8631718beaa97e78ee198567357c49fa2d4c164c
Topher <topherb99@gmail.com>
2020-04-18 15:06:21 +0100

    same bike docked is released

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,13 +1,14 @@
 require_relative 'bike'
 
 class DockingStation
+
+  attr_reader :bike
+
   def release_bike
     Bike.new
   end
 
-  def bike
-  end
-
   def dock(bike)
+    @bike = bike
   end
 end
